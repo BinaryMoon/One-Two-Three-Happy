a02a9b8c229abf096e6f77fe0691a4775bd5d5ed
BinaryMoon <binarymoon@gmail.com>
2018-11-27 22:08:54 +0000

    Allow the memories to be saved in localstorage

--- src/components/AddHappyMemories.js
+++ src/components/AddHappyMemories.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React from 'react';
 import HappyMemory from "./HappyMemory";
+import { getTodaysMemories } from '../helpers';
+import { addMemories } from "../helpers";
 
 
 /**
@@ -7,13 +9,55 @@ import HappyMemory from "./HappyMemory";
  */
 class AddHappyMemories extends React.Component {
 
+	memory1Ref = React.createRef();
+	memory2Ref = React.createRef();
+	memory3Ref = React.createRef();
+
+
+	/**
+	 * Save the new memories.
+	 */
+	formSubmit = ( event ) => {
+
+		event.preventDefault();
+
+		let memories = {
+			'memory1': this.memory1Ref.current.value,
+			'memory2': this.memory2Ref.current.value,
+			'memory3': this.memory3Ref.current.value
+		};
+
+		addMemories( memories );
+
+	};
+
+
 	render() {
 
+		let memories = getTodaysMemories();
+
 		return (
-			<form onSubmit={this.props.submitMemories}>
-				<HappyMemory id="memory-1">1.</HappyMemory>
-				<HappyMemory id="memory-2">2.</HappyMemory>
-				<HappyMemory id="memory-3">3.</HappyMemory>
+			<form onSubmit={this.formSubmit}>
+
+				<HappyMemory
+					formRef={this.memory1Ref}
+					id="memory-1"
+					number={1}
+					memory={memories.memory1}
+				/>
+				<HappyMemory
+					formRef={this.memory2Ref}
+					id="memory-2"
+					number={2}
+					memory={memories.memory2}
+				/>
+				<HappyMemory
+					formRef={this.memory3Ref}
+					id="memory-3"
+					number={3}
+					memory={memories.memory3}
+				/>
+
 				<input type="submit" value="Save Happy Memories" />
 			</form>
 		);
