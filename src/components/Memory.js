a07b73ba25012cb2144774cc610440f9808e66cc
BinaryMoon <binarymoon@gmail.com>
2018-11-28 19:31:00 +0000

    Display a nicer title on archive pages

--- src/components/Memory.js
+++ src/components/Memory.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { getDate } from '../helpers';
 
 class Memory extends React.Component {
 
 	render() {
 
+		let title = this.props.index;
+
+		if ( this.props.memories.date ) {
+			title = getDate( this.props.memories.date );
+		}
+
 		return (
 			<React.Fragment>
-				<h3>{ this.props.index }</h3>
+				<h3>{ title }</h3>
 				<ul>
 					<li>{ this.props.memories.memory1 }</li>
 					<li>{ this.props.memories.memory2 }</li>
