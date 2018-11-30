14d98108519411b0f4242948d1a72e611425d91c
BinaryMoon <binarymoon@gmail.com>
2018-11-30 10:32:05 +0000

    add a thumbs up emoji

--- src/openmoji.js
+++ src/openmoji.js
@@ -3,13 +3,15 @@ export const openmoji = {
 	'sparkles': '2728',
 	'star': '1F31F',
 	'happy': '1F601',
+	'thumb': '1F44D',
 };
 
 export const openmojiJoy = [
 	openmoji.rainbow,
 	openmoji.sparkles,
 	openmoji.star,
-	openmoji.happy
+	openmoji.happy,
+	openmoji.thumb,
 ];
 
 export function openmojiPath( image ) {
