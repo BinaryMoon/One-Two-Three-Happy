64936635cf880b4810753ccfbce96ee9290a7138
BinaryMoon <binarymoon@gmail.com>
2018-11-30 09:33:38 +0000

    swap " for ' cos ocd

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
-import React from "react";
-import Navigation from "./Navigation";
-import { Link } from "react-router-dom";
+import React from 'react';
+import Navigation from './Navigation';
+import { Link } from 'react-router-dom';
 
 const Header = (props) => (
 	<header>
