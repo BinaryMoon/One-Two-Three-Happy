9093a6375e082f8ccf21a1e89d95da908d17bf34
BinaryMoon <binarymoon@gmail.com>
2018-11-30 09:38:00 +0000

    add happy emojis

---
+++ src/openmoji.js
@@ -0,0 +1,16 @@
+export const openmoji = {
+	'rainbow': '1f308',
+	'sparkles': '2728',
+	'star': '1F31F',
+	'happy': '1F601',
+};
+
+export const openmojiJoy = [
+	openmoji.rainbow,
+	openmoji.sparkles,
+	openmoji.star,
+	openmoji.happy
+];
+
+
+
