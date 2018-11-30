4161f4e9dc5d925ec23a45f1558b635731295916
BinaryMoon <binarymoon@gmail.com>
2018-11-30 09:33:26 +0000

    Ensure memory's only display if there memorys to display

--- src/components/Memory.js
+++ src/components/Memory.js
@@ -6,7 +6,26 @@ class Memory extends React.Component {
 	render() {
 
 		let title = this.props.index;
+		let memories = this.props.memories;
+		let memoryHtml = [];
 
+		// Ensure the memories have been saved.
+		if ( ! memories.memory1 && ! memories.memory2 && ! memories.memory3 ) {
+			return null;
+		}
+
+		// Only display memories that are completed.
+		if ( memories.memory1 ) {
+			memoryHtml.push( <p>{ memories.memory1 }</p> );
+		}
+		if ( memories.memory2 ) {
+			memoryHtml.push( <p>{ memories.memory2 }</p> );
+		}
+		if ( memories.memory3 ) {
+			memoryHtml.push( <p>{ memories.memory3 }</p> );
+		}
+
+		// Work out what title to display.
 		if ( this.props.memories.date ) {
 			title = getDate( this.props.memories.date );
 		}
@@ -14,9 +33,7 @@ class Memory extends React.Component {
 		return (
 			<div className="memory">
 				<h3>{ title }</h3>
-				<p>{ this.props.memories.memory1 }</p>
-				<p>{ this.props.memories.memory2 }</p>
-				<p>{ this.props.memories.memory3 }</p>
+				{ memoryHtml }
 			</div>
 		);
 
