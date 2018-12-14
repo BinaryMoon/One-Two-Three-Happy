16a8f4565ac774c2bd4f0587d8a918de9a6a944a
BinaryMoon <binarymoon@gmail.com>
2018-12-14 19:45:39 +0000

    tidy about page html :)

--- src/screens/About.js
+++ src/screens/About.js
@@ -9,7 +9,6 @@ const About = () => (
 			<h1>About One Two Three Happy</h1>
 			<p>Keeping a daily gratitude journal can transform not only your mindset but the direction and impact of your life.</p>
 			<p>By keeping daily notes of the things that make you happy you will find yourself noticing happy events more often, in turn making you happier.</p>
-
 		</div>
 
 	</React.Fragment>
