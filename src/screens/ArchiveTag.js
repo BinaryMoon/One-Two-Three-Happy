0f9008f0f5d0c2ceae5fcafca1ba05af5da5fdd6
BinaryMoon <binarymoon@gmail.com>
2018-12-18 15:30:01 +0000

    Change tags page to use the archive screen

--- src/screens/ArchiveTag.js
+++ src/screens/ArchiveTag.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import Header from '../components/Header';
-import Archive from '../components/Archive';
-import { getMemoriesByTag } from '../memories';
+import Archives from './Archives';
+
 
 class ArchiveTag extends React.Component {
 
 	render() {
 
 		let params = this.props.match.params;
-		let memories = getMemoriesByTag( params.tag );
 
 		return (
-			<React.Fragment>
-				<Header />
-				<Archive
-					title={'Tag: <strong>' + params.tag + '</strong>'}
-					memories={memories}
-					tag={params.tag}
-				/>
-			</React.Fragment>
+			<Archives
+				title={'Tag: <strong>' + params.tag + '</strong>'}
+				tag={params.tag}
+			/>
 		);
 
 
