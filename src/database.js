8abb9a4c577e32196e433d7b12ca616d3ab4051e
BinaryMoon <binarymoon@gmail.com>
2018-12-18 15:27:01 +0000

    create functions for outputting the data tables to the console.

--- src/database.js
+++ src/database.js
@@ -2,11 +2,47 @@ import Dexie from 'dexie';
 
 const db = new Dexie( 'onetwothree' );
 
+
+/**
+ * Setup the tables.
+ */
 db.version(1).stores(
 	{
 		memories: 'id, emoji, memory, date',
 		tags: '++',
+		meta: 'key',
 	}
 );
+
+
+/**
+ * Export all table info to the console.
+ */
+export function databaseExport() {
+
+	databaseTableExport( 'memories' );
+	databaseTableExport( 'tags' );
+	databaseTableExport( 'meta' );
+
+}
+
+
+/**
+ * Export the specified table data.
+ *
+ * @param {string} table Table name.
+ */
+function databaseTableExport( table ) {
+
+	db
+		.table( table )
+		.toArray()
+		.then(
+			( data ) => {
+				// console.log( table, JSON.stringify( data ) );
+			}
+		);
+
+}
 
 export default db;
