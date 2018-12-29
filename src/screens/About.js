b4058a8ebf470ea59664f9f7b2e6378a46d74f8c
BinaryMoon <binarymoon@gmail.com>
2018-12-29 22:16:06 +0000

    Update About.js

--- src/screens/About.js
+++ src/screens/About.js
@@ -11,7 +11,7 @@ const About = () => (
 			<p>By keeping daily notes of the things that make you happy you will find yourself noticing happy events more often, in turn making you happier.</p>
 
 			<h2>Open Source</h2>
-			<p>I built OneTwoThreeHappy as a learning excercise. I wanted to learn React and this seemed like a relatively simple project to use as my first. As such, I thought I would build it in the open.</p>
+			<p>I built <strong>OneTwoThreeHappy</strong> as a learning excercise. I wanted to learn React and this seemed like a relatively simple project to use as my first. As such, I thought I would build it in the open.</p>
 			<p>The code for the OneTwoThreeHappy website is open source and can be found on <a href="https://github.com/BinaryMoon/One-Two-Three-Happy/">Github</a>. I am happy to receive issues and pull requests for the project.</p>
 
 			<h2>Credits</h2>
