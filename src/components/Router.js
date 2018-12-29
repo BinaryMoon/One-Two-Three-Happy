a2746d99eb40c13c4f4249f1ecd5f4f763642c36
BinaryMoon <binarymoon@gmail.com>
2018-12-29 22:08:08 +0000

    Code splitting on routes

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import NotFound from "../screens/NotFound";
+
 import Home from '../screens/Home';
-import About from '../screens/About';
 import Archives from '../screens/Archives';
 import ArchiveTag from '../screens/ArchiveTag';
 
+// import About from '../screens/About';
+const About = lazy( () => import( '../screens/About' ) );
+
 const Router = () => (
 	<BrowserRouter>
-		<Switch>
-			<Route exact path="/" component={Home} />
-			<Route exact path="/archive/" component={Archives} />
-			<Route exact path="/about/" component={About} />
-			<Route exact path="/tag/:tag/" component={ArchiveTag} />
-			<Route component={NotFound} />
-		</Switch>
+		<Suspense fallback={<div>Loading...</div>}>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/archive/" component={Archives} />
+				<Route exact path="/about/" component={About} />
+				<Route exact path="/tag/:tag/" component={ArchiveTag} />
+				<Route component={NotFound} />
+			</Switch>
+		</Suspense>
 	</BrowserRouter>
 );
 
 export default Router;
+
+
